8ef6073c9a9df7a7ff36380112d922c36f8b03d4
Jake O'Shannessy <joshannessy@gmail.com>
2018-03-08 17:51:09 +0300

    lua: Cleanup of csv parser

--- Source/smokeview/csv.c
+++ Source/smokeview/csv.c
@@ -50,61 +50,65 @@ char *simpleunquote(char *string){
 size_t readcsv(char *file, dvector **dvectors){
   FILE *stream;
   fprintf(stderr, "reading: %s\n", file);
+  // Open the file.
   if((stream = fopen(file, "r")) == NULL)return 0;
-  // char buffer[BUFFERSIZE];
+  // Allocate a buffer for streaming.
   char *buffer = malloc(BUFFERSIZE*sizeof(char));
 
   // Read data into buffer.
   if(fgets(buffer, BUFFERSIZE, stream) == NULL)return 0;
-  // char bufferdup[BUFFERSIZE];
-  char *bufferdup = malloc(BUFFERSIZE*sizeof(char));
-  strcpy(bufferdup, buffer);
+
+  // Copy this data into a duplicate buffer, which will be used to count the
+  // number of vectors.
+  char *countbuffer = malloc(BUFFERSIZE*sizeof(char));
+  strcpy(countbuffer, buffer);
   // Trim buffer to a single line.
-  // TODO: what about windows, fortran is usually just LF, but check
+  // TODO: this offers no escaping
+  // This trims both the original and the duplicate
   buffer[strcspn(buffer, "\n")] = 0;
-  bufferdup[strcspn(buffer, "\n")] = 0;
-  fprintf(stderr, "buffer: >%s<\n", buffer);
-  fprintf(stderr, "bufferdup: >%s<\n", bufferdup);
-  // The first line is units, count the number of units first.
+  countbuffer[strcspn(buffer, "\n")] = 0;
+
+  // The first line is units, count the number of units first. This is done
+  // using the countbuffer.
   char *bufptr;
   size_t x = 0;
-  bufptr = strsep(&buffer, ",\n");
+  bufptr = strsep(&countbuffer, ",\n");
   while(bufptr != NULL) {
-    fprintf(stderr, "unit[%d]: %s\n", x, bufptr);
     x++;
-    bufptr = strsep(&buffer, ",\n");
+    bufptr = strsep(&countbuffer, ",\n");
   }
+  // We no longer need the count buffer.
+  free(countbuffer);
   size_t n = x;
   // Now we know how many vectors we have. Assume the first vector is our
-  // x vector
+  // x vector.
   // Initialise memory for each
   vector *vectors = malloc(n*sizeof(vector));
 
-  // Read through the units line again and set the unit name values.
-  // Assume the first vector is the x-vector (usually time)
-  bufptr = strsep(&bufferdup, ",\n");
+  // Read through the units line again (using the main buffer) and set the unit
+  // name values. Assume the first vector is the x-vector (usually time).
+  bufptr = strsep(&buffer, ",\n");
   size_t j = 0;
   while(bufptr != NULL) {
-    fprintf(stderr, "unitsbufptr[%d]: %s\n", j, bufptr);
     strcpy(vectors[j].units, bufptr);
-    bufptr = strsep(&bufferdup, ",\n");
+    bufptr = strsep(&buffer, ",\n");
     j++;
   }
+  // Read data into buffer for names (the second line). This only reads up to
+  // a line (or max buffer size)
   free(buffer);
+  // TODO: why are we reallocating this?
   buffer = malloc(BUFFERSIZE*sizeof(char));
-  // Read data into buffer for names.
   if(fgets(buffer, BUFFERSIZE, stream) == NULL){
     fprintf(stderr, "fgets failed\n");
     return 0;
   }
   // Trim buffer to a single line.
-  // TODO: what about windows, fortran is usually just LF, but check
   buffer[strcspn(buffer, "\n")] = 0;
   // Assume the first vector is the x-vector (usually time)
   bufptr = strsep(&buffer, ",\n");
   j = 0;
   while(bufptr != NULL) {
-    fprintf(stderr, "name[%d]: %s\n", j, bufptr);
     strcpy(vectors[j].name, simpleunquote(bufptr));
     bufptr = strsep(&buffer, ",\n");
     j++;
