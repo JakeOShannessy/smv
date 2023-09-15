eaa320df340bb55a02dd7b89e6b55a437030c486
Jake O'Shannessy <joshannessy@gmail.com>
2023-09-15 20:46:36 +0100

    add check and format to makefile

---
+++ Build/smokeview/gnu_linux_64/format_smokeview.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ../../scripts/setopts.sh $*
+
+# Exit immediately if any of the build steps fail
+set -e
+
+curdir=`pwd`
+LIBDIR=../../LIBS/gnu_linux_64/
+eval make -j 4 ${SMV_MAKE_OPTS} -f ../Makefile gnu_linux_64_format
