5c90433c0b5fa9903a9925e0143b321dff890854
Jake O'Shannessy <joshannessy@gmail.com>
2024-03-06 21:21:36 +0000

    split smoke reading function and move to shared
    As with readimage.c/h there's a lot of useful code that is currently
    intertwined with GLUT and GLUI. This splits some smoke reading code and
    places it in shared.
    
    This is 3/7 patches that are required to parse and post-process .smv
    files without GLUT/GLUI etc.

---
+++ Source/shared/readsmoke.h
@@ -0,0 +1,31 @@
+#ifndef READSMOKE_H_DEFINED
+#define READSMOKE_H_DEFINED
+#include "options.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+// #if defined(WIN32)
+// #include <windows.h>
+// #endif
+// #include GLU_H
+
+// #include "datadefs.h"
+// #include "smokeviewvars.h"
+// #include "IOobjects.h"
+// #include "getdata.h"
+#include "MALLOCC.h"
+#include "contourdefs.h"
+#include "histogram.h"
+#include "smokeviewdefs.h"
+#include "string_util.h"
+#include "isodefs.h"
+#include "structures.h"
+
+#include "readobject.h"
+
+
+void FreeSmoke3D(smoke3ddata *smoke3di);
+#endif
--- Source/smokeview/IOsmoke.c
+++ Source/smokeview/IOsmoke.c
@@ -3762,34 +3762,6 @@ int GetSmoke3DSizes(smoke3ddata *smoke3di, int fortran_skip, char *smokefile, in
   return 0;
 }
 
-/* ------------------ FreeSmoke3d ------------------------ */
-
-void FreeSmoke3D(smoke3ddata *smoke3di){
-
-  smoke3di->lastiframe = -999;
-#ifdef pp_SMOKEFRAME
-  FRAMEFree(smoke3di->frameinfo);
-  smoke3di->frameinfo = NULL;
-#endif
-  FREEMEMORY(smoke3di->smokeframe_in);
-  FREEMEMORY(smoke3di->smokeframe_out);
-  FREEMEMORY(smoke3di->timeslist);
-  FREEMEMORY(smoke3di->times);
-  FREEMEMORY(smoke3di->times_map);
-  FREEMEMORY(smoke3di->use_smokeframe);
-  FREEMEMORY(smoke3di->nchars_compressed_smoke_full);
-  FREEMEMORY(smoke3di->nchars_compressed_smoke);
-  FREEMEMORY(smoke3di->frame_all_zeros);
-  FREEMEMORY(smoke3di->smoke_boxmin);
-  FREEMEMORY(smoke3di->smoke_boxmax);
-#ifndef pp_SMOKEFRAME
-  FREEMEMORY(smoke3di->smoke_comp_all);
-#endif
-  FREEMEMORY(smoke3di->smokeframe_comp_list);
-  FREEMEMORY(smoke3di->smokeview_tmp);
-  FREEMEMORY(smoke3di->smokeframe_loaded);
-}
-
 /* ------------------ GetSmoke3DVersion ------------------------ */
 
 #ifdef pp_SMOKE_SPEEDUP
