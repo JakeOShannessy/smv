dadb20b78770fbc012c4218390685ee1c9c62767
Jake O'Shannessy <joshannessy@gmail.com>
2024-08-28 20:35:09 +0100

    write obsts table

--- Tests/create_hdf5.c
+++ Tests/create_hdf5.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "hdf5_hl.h"
 #include <hdf5.h>
-#include <hdf5_hl.h>
 
 herr_t add_dataset(hid_t file_id) {
   herr_t status = 0;
@@ -238,6 +238,88 @@ fail:
   return status;
 }
 
+typedef struct _blockagedata {
+  int ijk[6];
+  char *name;
+  float xmin, xmax, ymin, ymax, zmin, zmax;
+} blockagedata;
+
+herr_t add_obsts_list(hid_t file_id) {
+  herr_t status = 0;
+  // Create the dataspace for the dataset.
+  hsize_t dims[3];
+  dims[0] = 4;
+  dims[1] = 6;
+  dims[2] = 6;
+#define N_FIELDS 5
+#define N_RECORDS 10
+  const char *field_names[N_FIELDS] = {"a", "b", "c", "d", "e"};
+  const size_t offsets[N_FIELDS] = {
+      HOFFSET(blockagedata, xmin), HOFFSET(blockagedata, xmax),
+      HOFFSET(blockagedata, ymin), HOFFSET(blockagedata, ymax),
+      HOFFSET(blockagedata, zmin)};
+  const hid_t field_types[N_FIELDS] = {
+      H5T_NATIVE_FLOAT, H5T_NATIVE_FLOAT, H5T_NATIVE_FLOAT,
+      H5T_NATIVE_FLOAT, H5T_NATIVE_FLOAT,
+  };
+  blockagedata blockages[N_RECORDS];
+  for(int i = 0; i < N_RECORDS; i++) {
+    blockages[i].ijk[0] = 1;
+    blockages[i].ijk[1] = 1;
+    blockages[i].ijk[2] = 1;
+    blockages[i].ijk[3] = 1;
+    blockages[i].ijk[4] = 1;
+    blockages[i].ijk[5] = 1;
+    blockages[i].name = "ObstName";
+    blockages[i].xmin = 1.0;
+    blockages[i].xmax = 1.0;
+    blockages[i].ymin = 1.0;
+    blockages[i].ymax = 1.0;
+    blockages[i].zmin = 1.0;
+    blockages[i].zmax = 1.0;
+  }
+  status = H5TBmake_table("Obstructions", file_id, "Obsts", N_FIELDS, N_RECORDS,
+                          sizeof(blockagedata), field_names, offsets,
+                          field_types, 5, 0, 0, blockages);
+
+  // // Create the dataset.
+  // hid_t dataset_id = H5Dcreate(file_id, "/slice", H5T_IEEE_F64LE,
+  // dataspace_id,
+  //                              H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+  // double dset_data[144];
+  // for(size_t i = 0; i < 4; i++) {
+  //   for(size_t j = 0; j < 6; j++) {
+  //     for(size_t k = 0; k < 6; k++) {
+  //       size_t index = i * 6 * 6 + j * 6 + k;
+  //       dset_data[index] = i * 4 + j + 23;
+  //     }
+  //   }
+  // }
+  // status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
+  //                   H5P_DEFAULT, dset_data);
+  // if(status < 0) goto fail;
+  // status = H5Dread(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL,
+  // H5P_DEFAULT,
+  //                  dset_data);
+  // if(status < 0) goto fail;
+
+  // status = set_quantity(dataset_id, "VELOCITY");
+  // status = set_short_name(dataset_id, "vel");
+  // status = set_units(dataset_id, "m/s");
+  // status = add_double(dataset_id, "Time", 15.0);
+  // status = add_int(dataset_id, "Mesh", 2);
+  // int ijk[6] = {2, 6, 6, 12, 6, 12};
+  // status = set_bounds(dataset_id, ijk);
+  // if(status < 0) goto fail;
+
+  // Close the dataset and dataspace
+fail:
+  // H5Dclose(dataset_id);
+  // H5Sclose(dataspace_id);
+  return status;
+}
+
 herr_t add_fds_version(hid_t file_id) {
   return add_string_attribute(file_id, "FDSVersion",
                               "FDS-6.8.0-0-g886e009-release");
@@ -264,6 +346,9 @@ int main() {
   if(status < 0) goto fail;
   status = add_albedo(file_id);
   if(status < 0) goto fail;
+  add_string_attribute(file_id, "Title", "Single Couch Test Case");
+  add_int(file_id, "NMeshes", 4);
+  add_obsts_list(file_id);
 fail:
   status = H5Fclose(file_id);
   return status;
