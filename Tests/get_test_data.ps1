1bf60494a20aa31844915c617104138ec5b82fcf
Jake O'Shannessy <joshannessy@gmail.com>
2024-09-03 09:49:36 +0100

    cmake: use cmake to fetch test data

--- Tests/get_test_data.ps1
+++
@@ -1,9 +0,0 @@
-$commit_fig = "374d9db0062fd6dc38b30bc0d27cd84c9bb2b69d"
-
-$zip_path = "$env:temp\test-data.zip"
-$unzip_path = "$env:temp\test-data-out"
-Invoke-WebRequest -Uri https://github.com/firemodels/fig/archive/$commit_fig.zip -OutFile $zip_path
-Expand-Archive $zip_path -DestinationPath $unzip_path -Force
-mkdir fig -Force
-Move-Item $unzip_path/fig-$commit_fig/* fig -Force
-Copy-Item -Force -Recurse fig/smv/Tests/Visualization ../Verification
