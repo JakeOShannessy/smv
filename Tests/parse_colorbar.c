760c312669a3161f3ce5864a0b7a0e38d1f13203
Jake O'Shannessy <joshannessy@gmail.com>
2025-03-24 22:32:21 +0000

    print results from tests

--- Tests/parse_colorbar.c
+++ Tests/parse_colorbar.c
@@ -16,13 +16,14 @@ char append_string[1024];
 
 int main(int argc, char **argv) {
   initMALLOC();
-  if (argc < 2) return 2;
+  if(argc < 2) return 2;
   int error = 0;
   const char *filename = argv[1];
   colorbardata *cb;
   NEWMEMORY(cb, sizeof(colorbardata));
 
   int result = ReadCSVColorbar(cb, filename, "divergent", CB_DIVERGENT);
+  fprintf(stderr, "result: %i\n", result);
   assert(!result);
   assert(strcmp(cb->colorbar_type, "divergent") == 0);
   assert(strcmp(cb->menu_label, "light blue->white->light red") == 0);
