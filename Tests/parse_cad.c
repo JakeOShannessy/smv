6f885278ab2a86df7989d50c1e98df39a8598f0e
Jake O'Shannessy <joshannessy@gmail.com>
2025-03-25 09:30:40 +0000

    simplify option declarations

--- Tests/parse_cad.c
+++ Tests/parse_cad.c
@@ -6,11 +6,6 @@
 
 #include <assert.h>
 
-int show_help;
-int hash_option;
-int show_version;
-char append_string[1024];
-
 int main(int argc, char **argv) {
   if(argc < 2) {
     fprintf(stderr, "error: insufficient arguments");
--- Tests/parse_objects.c
+++ Tests/parse_objects.c
@@ -9,11 +9,6 @@
 
 #include "readobject.h"
 
-int show_help;
-int hash_option;
-int show_version;
-char append_string[1024];
-
 int main(int argc, char **argv) {
   initMALLOC();
   if (argc > 1) {
