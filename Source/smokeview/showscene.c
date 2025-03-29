9855932a9ea08f3287bd80fd268fa713f7581529
Jake O'Shannessy <joshannessy@gmail.com>
2025-03-29 10:44:56 +0000

    add scase arg to DrawNorth

--- Source/smokeview/showscene.c
+++ Source/smokeview/showscene.c
@@ -64,7 +64,7 @@ void ShowScene2(int mode){
 
     if(vis_northangle == 1){
       CLIP_GEOMETRY;
-      DrawNorth();
+      DrawNorth(&global_scase);
       SNIFF_ERRORS("after DrawNorth");
     }
 
