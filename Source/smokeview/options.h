eaa6a7407639903a46b59a97bf6fcd54c351f24a
Jake O'Shannessy <joshannessy@gmail.com>
2025-03-30 13:19:25 +0100

    replace pp_LINUX with __linux__

--- Source/smokeview/options.h
+++ Source/smokeview/options.h
@@ -19,7 +19,7 @@
 
 //*** options: Linux
 
-#ifdef pp_LINUX
+#ifdef __linux__
 #define pp_REFRESH          // refresh glui dialogs when they change size
 #define pp_DIALOG_SHORTCUTS // dialog shortcuts
 #endif
