1d1814cc47352db8309ab8661207e934a0997068
Jake O'Shannessy <joshannessy@gmail.com>
2025-08-25 11:05:52 +0100

    win: add support for unicode files
    On linux paths are bytestrings and UTF-8 is supported transparently. On
    Windows, Unicode paths are a superset of UTF-16. This means that
    Smokeview can have issues opening files and directories with non-ASCII
    characters in their path.
    
    This adds a conversion function from UTF-8 to UTF-16 and includes it in
    the FOPEN, UNLINK, CHDIR, and MKDIR functions. The functions are also
    moved to have more consistent definitions.
    
    Tests are added to verify that this works correctly.
    
    This also adds Unicode support to commandline arguments.
    
    Previously on Windows when running Smokeview from the commandline, any
    path that included Unicode characters could not be opened.
    
    This adds a function for getting the commandline arguments. On
    non-Windows platforms this just returns the values unchanged. On Windows
    it uses Windows-specific function to retrieve Unicode versions of the
    arguments and convert them to an appropriate version.

---
+++ Tests/test_iter_dir.c
@@ -0,0 +1,31 @@
+#include "options.h"
+
+#include "getdata.h"
+
+#include "dmalloc.h"
+
+#include "file_util.h"
+#include "string_util.h"
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <stdio.h>
+#include <strsafe.h>
+#include <tchar.h>
+#include <windows.h>
+#pragma comment(lib, "User32.lib")
+
+int show_help;
+int hash_option;
+int show_version;
+char append_string[1024];
+
+int main(int argc, char **argv) {
+  int mode = FILE_MODE;
+  initMALLOC();
+  int t2 = GetFileListSize("Utilities/Scripts", "*", mode);
+  filelistdata *l2 = NULL;
+  MakeFileList("Utilities/Scripts", "*", t2, NO, &l2, mode);
+  return 0;
+}
