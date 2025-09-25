6a112554da22abae6bececb2334bf90b41c1101f
Jake O'Shannessy <joshannessy@gmail.com>
2025-09-25 12:58:19 +0100

    Gifs (#10)
    * add option to create gifs
    
    This is similar to the move making option but produces an anuimated GIF.
    It uses the linked GD library rather than calling a separate program.
    
    This can be accessed via the render dialog box (adjacent the movie
    options).
    
    There are still improvements to be made namely:
    
    - Adding more options (framerate, render specific frames).
    - Adding access to scripts.
    
    GIFs have a limited colour palette, as such stepped colourbars (or
    similar) should be used to reduce the number of output colours.
    
    This colour limitation is an issue for detailed results but this is
    useful for debugging purposes or simple concepts.
    
    * enable/disable gifs based on libgd version
    
    * place GIF dialog buttons behind libgd version gate
    
    * factor out pp_GIF_ANIMATED and add docs
    
    ---------
    
    Co-authored-by: Jake O'Shannessy <joshanessy@threadway.com>

--- Source/smokeview/menus.c
+++ Source/smokeview/menus.c
@@ -2104,6 +2104,24 @@ void RenderMenu(int value){
   case RenderStart360:
     RenderCB(RENDER_START_360);
     break;
+
+#if pp_GIF_ANIMATED
+  case RenderStartGIF:
+    render_mode = RENDER_GIF;
+    resolution_multiplier=1;
+    {
+      char *gif_filename;
+      NEWMEMORY(gif_filename, strlen(global_scase.chidfilebase) + 4 + 1);
+      strcpy(gif_filename, global_scase.chidfilebase);
+      strcat(gif_filename, ".gif");
+      char *gif_filepath = CombinePaths(".", gif_filename);
+      FREEMEMORY(gif_filename);
+      GifStart(gif_filepath);
+      FREEMEMORY(gif_filepath);
+    }
+    RenderMenu(RenderStart);
+    break;
+#endif
   case RenderStartORIGRES:
     render_mode = RENDER_NORMAL;
     resolution_multiplier=1;
